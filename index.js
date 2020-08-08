da31a76f6790fce788c1653badf258878742c0e4
achilj <39186535+achilj@users.noreply.github.com>
2020-08-08 05:42:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.login(process.env.token);
 client.on("ready", async () =>{
 
     console.log(`${client.user.username} Is Online!`);
-    client.user.setActivity("/help | MaanstadRP", {type: "WATCHING"})
+    client.user.setActivity("|MaanstadRP|", {type: "WATCHING"})
 
 });
 

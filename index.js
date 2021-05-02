bf8f53ab9f0ea9a7eb7a2df48439a3841345b7ec
achilj <39186535+achilj@users.noreply.github.com>
2021-05-02 16:44:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setPresence({
         status: "dnd",  // You can show online, idle... Do not disturb is dnd
-        game: {
-            name: "!help",  // The message shown
-            type: "PLAYING" // PLAYING, WATCHING, LISTENING, STREAMING,
-        }
     });
- });
+    
+client.user.setActivity("with depression", {
+  type: "STREAMING",
+  url: url here"
+});  
+});

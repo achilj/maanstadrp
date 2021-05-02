0cc906346175fba32a809b3e7df4be16f444fa80
achilj <39186535+achilj@users.noreply.github.com>
2021-05-02 16:56:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ client.login(process.env.token);
 client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setPresence({status: "dnd"})//;
-    //client.user.setActivity("Hans van MaanstadRP", {type: "WATCHING"})
+    client.user.setActivity("Hans van MaanstadRP", {type: "WATCHING"})
     });
